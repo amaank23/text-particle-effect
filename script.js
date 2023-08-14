318c10d2a5200f42ad0b867c977686e442a41ad0
unknown <amaanwaseem24@gmail.com>
2023-08-13 19:11:06 -0700

    first commit

---
+++ script.js
@@ -0,0 +1,64 @@
+window.addEventListener("load", () => {
+  const textInput = document.getElementById("textInput");
+  const canvas = document.getElementById("canvas");
+  const ctx = canvas.getContext("2d");
+  canvas.width = window.innerWidth;
+  canvas.height = window.innerHeight;
+
+  ctx.lineWidth = 3;
+  ctx.strokeStyle = "red";
+  ctx.beginPath();
+  ctx.moveTo(canvas.width / 2, 0);
+  ctx.lineTo(canvas.width / 2, canvas.height);
+  ctx.stroke();
+
+  ctx.lineWidth = 3;
+  ctx.strokeStyle = "green";
+  ctx.beginPath();
+  ctx.moveTo(0, canvas.height / 2);
+  ctx.lineTo(canvas.width, canvas.height / 2);
+  ctx.stroke();
+
+  const gradient = ctx.createLinearGradient(0, 0, canvas.width, canvas.height)
+  gradient.addColorStop(0.3, "red")
+  gradient.addColorStop(0.5, "orange")
+  gradient.addColorStop(0.7, "yellow")
+  ctx.fillStyle = gradient;
+  ctx.strokeStyle = "white";
+
+  ctx.lineWidth = 3;
+  ctx.font = "80px Helvetica";
+  ctx.textAlign = "center";
+  ctx.textBaseline = "middle";
+
+  const maxTextWidth = canvas.width * 0.5;
+  const lineHeight = 80;
+
+  function wrapText(text) {
+    let linesArray = [];
+    let lineCounter = 0;
+    let line = "";
+    let words = text.split(" ");
+
+    for (let i = 0; i < words.length; i++) {
+      let testLine = line + words[i] + " ";
+      if (ctx.measureText(testLine).width > maxTextWidth) {
+        line = words[i] + " ";
+        lineCounter++;
+      } else {
+        line = testLine;
+      }
+      linesArray[lineCounter] = line;
+    }
+    let textHeight = lineHeight * lineCounter;
+    let textY = canvas.height / 2 - textHeight / 2;
+    linesArray.forEach((el, index) => {
+      ctx.fillText(el, canvas.width / 2, textY + index * lineHeight);
+    });
+  }
+
+  textInput.addEventListener("keyup", (e) => {
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
+    wrapText(e.target.value);
+  });
+});
